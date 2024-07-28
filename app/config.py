5de9a8b3d0f6ebb0a406ee23201cdceda6ba397b
Slava Kuryaev <92830706+necrosskull@users.noreply.github.com>
2024-07-29 00:55:57 +0300

    fix: .env parse

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 from functools import lru_cache
 
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
@@ -20,6 +20,8 @@ class Settings(BaseSettings):
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     REFRESH_TOKEN_EXPIRE_DAYS: int
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 
 @lru_cache
